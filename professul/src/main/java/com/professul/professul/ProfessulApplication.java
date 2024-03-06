cfd16e804a9c836f39066a778939a44140592985
JuhyeonLee1 <zugusdl@gmail.com>
2024-03-06 13:45:59 +0900

    setting

---
+++ professul/src/main/java/com/professul/professul/ProfessulApplication.java
@@ -0,0 +1,13 @@
+package com.professul.professul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfessulApplication.class, args);
+    }
+
+}
