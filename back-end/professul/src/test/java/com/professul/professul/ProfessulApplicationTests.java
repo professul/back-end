c861f0e7ffdf507a0c9de6aa53abcc119c085faf
JuhyeonLee1 <zugusdl@gmail.com>
2024-03-06 12:04:19 +0900

    project setting

---
+++ back-end/professul/src/test/java/com/professul/professul/ProfessulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.professul.professul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfessulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
